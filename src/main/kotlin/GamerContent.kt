7c2c6e55818a3c0a21c74e092df4d1987111248d
Dogan Mike Turan <doganmiketuran@live.dk>
2019-12-18 23:34:35 +0100

    sdsd

---
+++ src/main/kotlin/GamerContent.kt
@@ -0,0 +1,39 @@
+open class GamerContent : WebContent {
+
+    val Gamer1 = Gamer(1,"Tony", 100)
+    val Gamer2 = Gamer(2,"Mark",125)
+    val Gamer3 = Gamer(3,"Hans",155)
+
+
+    val myList = mutableListOf<Gamer>(Gamer1,Gamer2,Gamer3)
+    override fun getGamer(): List<Gamer> {
+        return myList
+    }
+
+    override fun getGamer(id: Int): Gamer {
+       myList.forEach { g ->
+           if (g.id == id) {
+               return g;
+           }
+       }
+
+        return Gamer(0, "Not found", 0);
+    }
+
+    override fun putGamer(value: Gamer): Gamer {
+        myList.add(value);
+        return value;
+    }
+
+    override fun deleteGamer(id: Int): Gamer {
+        var deletingGamer = Gamer(0, "Not found", 0)
+        for (gamer in myList) {
+            if(gamer.id == id){
+                myList.remove(gamer)
+                deletingGamer = gamer
+            }
+        }
+        return deletingGamer
+    }
+
+}
